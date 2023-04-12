63ad67f25734c8276816494d4395e7f1dc12bbb2
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-04-12 12:01:19 +0600

    page add & name routing

---
+++ lib/pages/user_list_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UserListPage extends StatelessWidget {
+  const UserListPage({Key? key}) : super(key: key);
+  static const String routeName = '/UserListPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
